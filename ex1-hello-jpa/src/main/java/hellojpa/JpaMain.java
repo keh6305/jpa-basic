acae50fb94fac75ff79c6939acacb7e677865144
김은호 <>
2025-08-05 12:30:16 +0900

    간단한 CRUD

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,15 +2,55 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
-
     public static void main(String[] args) {
-
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
+        EntityTransaction tx = em.getTransaction();
 
-        em.close();
+        tx.begin();
+
+        try {
+            /* Member 등록
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("HelloB");
+
+            em.persist(member);
+            */
+
+            // Member 단일 조회
+//            Member findMember = em.find(Member.class, 1L);
+//            System.out.println("findMember.getId() = " + findMember.getId());
+//            System.out.println("findMember.getName() = " + findMember.getName());
+
+            // Member 수정
+//            findMember.setName("HelloJPA");
+
+            // Member 삭제
+//            em.remove(findMember);
+
+            // Member 목록 조회
+            List<Member> members = em.createQuery("SELECT m FROM Member AS m", Member.class)
+                    .setFirstResult(0)
+                    .setMaxResults(10)
+                    .getResultList();
+
+            for (Member member : members) {
+                System.out.println("member.getName() = " + member.getName());
+            }
+
+            tx.commit();
+        }
+        catch (Exception e) {
+            tx.rollback();
+        }
+        finally {
+            em.close();
+        }
+
         emf.close();
     }
 }
