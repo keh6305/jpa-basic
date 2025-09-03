504c226f05acafd049af94ddddce2d4a895abf37
김은호 <keh6305@gmail.com>
2025-09-04 00:04:50 +0900

    상품 기능

---
+++ jpa-ex2/ex2-jpashop/src/main/java/com/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,45 @@
+package com.jpabook.jpashop.repository;
+
+import com.jpabook.jpashop.domain.item.Item;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+    private final EntityManager em;
+
+    /**
+     * 상품 저장
+     * @param item
+     */
+    public void save(Item item) {
+        if (item.getId() == null) {
+            em.persist(item);
+        }
+        else {
+            em.merge(item);
+        }
+    }
+
+    /**
+     * 상품 단일 조회
+     * @param id
+     * @return
+     */
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    /**
+     * 상품 전체 조회
+     * @return
+     */
+    public List<Item> findAll() {
+        return em.createQuery("SELECT i FROM Item i ", Item.class)
+                .getResultList();
+    }
+}
